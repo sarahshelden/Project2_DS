baaf157018abf480c3a0285a9432ae23f50f28c1
Sarah Shelden <sarah.shelden113@gmail.com>
2019-04-11 19:54:49 -0400

    navbar

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Plot from "./components/plots";
+import NavBar from "./components/navbar";
 
 class App extends React.Component {
   render() {
     return (
       <div>
+         <NavBar/>
         <Plot/> 
+       
       </div>
       
     );
